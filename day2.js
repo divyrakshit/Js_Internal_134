a6c0543ce5c40607f844d43be2d2b3e8591d5928
divyrakshit <divyrakshit07@gmail.com>
2025-12-09 06:05:52 +0000

    commit

---
+++ day2.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// let a=23;
+// console.log(a)
+// const b="abc";
+// console.log(b)
+// var c=78;
+// console.log(c)
+// d=88;
+// console.log(d)
+
+
+
+// let a="abc"
+// console.log(typeof(a))
+// let b=""
+// console.log("Value of b is" +" "+ b)
+// let c=null
+// console.log("Value of c is" + " " +c)
+// let d;
+// console.log(typeof(d))
+// console.log(typeof(null))
+
+
+let score="33abscd";
+console.log(typeof(score));
+console.log(typeof score);
+let scoe=Number(score);
+console.log(scoe);
