91cc54bfc6cd3bbaa730d0dc3574804a13bb1b1e
divyrakshit <divyrakshit07@gmail.com>
2025-12-10 05:35:15 +0000

    commit

---
+++ day3.js
@@ -0,0 +1,66 @@
+// function run(n)
+// {
+//     return n*2;
+// }
+// let ch=run(72);
+// console.log(ch)
+
+
+// const double = (x) => x * 2;
+// console.log(double(72));
+
+// function double(x)
+// {
+// return x * 2;
+// }
+
+// let ch = double(72);
+// console.log(ch);
+
+//  let fruits = ["apple","orange","banana"]
+//  console.log(...fruits)
+
+// let fruits = ["apple","orange","banana","mango","pear"]
+
+//  console.log(...fruits)
+
+//  fruits.forEach(fruits => console.log(fruits))
+
+//  fruits.push("blackberry")
+//  console.log (...fruits)
+
+//  fruits.pop()
+//  console.log (...fruits)
+
+//  fruits.unshift("kivi")
+//  console.log (...fruits)
+
+//  fruits.shift()
+//  console.log (...fruits)
+
+
+// map operation
+// let num=[1,2,3]
+// let doubled=num.map(n=>n*2)
+// console.log(doubled)
+
+// // filter() operation
+// let nums=[10,25,30,40]
+// let result=nums.filter(n=>n>20);
+// console.log(result)
+
+// reduce() operation
+// let arr = [2, 4, 6, 8];
+
+// // Map → multiply each number by 5
+// let doubled = arr.map(n => n * 5);
+// console.log(doubled);   // [10, 20, 30, 40]
+
+// // Filter → numbers greater than 20
+// let result = doubled.filter(n => n > 20);
+// console.log(result);   // [30, 40]
+
+// // Reduce → sum of array
+// let number = [1, 2, 3, 3, 4];
+// let tot = number.reduce((acc, val) => acc + val, 0);
+// console.log(tot);      // 13
